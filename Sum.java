c14d3c26242752c5b9b78bdc7e58520807ea5e6d
Rebecca Moss <moss.rebecca.10@gmail.com>
2023-10-24 12:37:18 +0200

    ændret die så man kan bestem max og min uden for class

--- Sum.java
+++ Sum.java
@@ -1,11 +1,16 @@
+import javax.management.relation.Role;
+
 class Sum{
     public int sum;
-    public Sum(){
+
+   public Sum(){
         int sum = 0;
         int numberOfDice = 2;
         int[] arr = new int[numberOfDice]; //made an array of dice to show it is easy to change the number of dice
         for (int i = 0; i<arr.length; i++){
-            arr[i] = new Dice().getValue();
+            Die d1 = new Die(1, 6);
+            d1.roll();
+            arr[i] = d1.getFaceValue();
             sum += arr[i];
         }
         this.sum = sum;
