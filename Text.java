829c84542172fc633e01df8afc938ca959df4278
S235119 <S235119@dtu.dk>
2023-10-26 10:30:18 +0200

    flavortext added

--- Text.java
+++ Text.java
@@ -14,17 +14,17 @@ class Text{
     };
 
     private static String[] text = new String[]{
-        "Text 1",
-        "Text 2",
-        "Text 3",
-        "Text 4",
-        "Text 5",
-        "Text 6",
-        "Text 7",
-        "Text 8",
-        "Text 9",
-        "Text 10",
-        "Text 11",
+        "You reach the tower, manage to climb up the vines along the side, and enter the" + "\n" + "chamber through the window. You look under the mattress and find 250 gold hidden" + "\n" + "underneath. You run home to celebrate your find.",
+        "As you’re running along you suddenly slip and fall into a crater. After your fall you faint," + "\n" + "and wake up moments later surrounded by orcs, they rob you of everything you have," + "\n" + "including your clothes, and finally leave. You run home, naked and afraid. You lost 100" + "\n" + "gold.",
+        "You get to the palace gates and everything is looking great, you look around and find a" + "\n" + "wallet on the floor. You decide to take what’s in it, and find 100 gold. You go in and drink to" + "\n" + "celebrate, and wake up in the morning, somehow, at home.",
+        "You wander into the cold desert, and enter the pub to get out of the cold. You spend the" + "\n" + "entire night drinking, and when you wake up, you notice that you’ve lost 20 gold, and there" + "\n" + "is an unfamiliar smell on your sheets.",
+        "As you’re taking a walk, you see a walled city, you choose to approach them. When the" + "\n" + "townsfolk notice you they are frightened, and bribe you with 180 gold to leave them alone," + "\n" + "and not to tell others of their existence. Happy with the money, you take it, and find your" + "\n" + "way home.",
+        "You decide to visit the monastery to repent all the sins you’ve committed in your life." + "\n" + "You go and pray, and when they ask for donations, you pretend to not understand and" + "\n" + "quickly hurry home, there is a limit to how much you repent.",
+        "On one of your adventures you notice a black cave, and as the brilliant adventurer you" + "\n" + "are, you decide to investigate. In the cave you see a chest and decide to rob it. When you" + "\n" + "approach it, it turns out to be a mimic, and attacks you. Once you manage to escape, you" + "\n" + "drop your bag on your way out, and unfortunately lose 70 gold.",
+        "You’re out in the mountain looking for food, and notice a small collection of huts, they" + "\n" + "look abandoned. You choose to go look through them in hopes of loot. After going through" + "\n" + "all of them you’ve managed to collect 60 gold, and a bit of food. You return home happy" + "\n" + "and hungry.",
+        "You stumble across the Werewall on your latest adventure. It’s a terrifying sight, with" + "\n" + "werewolves everywhere. They notice your smell, and start chasing you. As you’re running," + "\n" + "one of them reaches you, and manages to snatch your pouch. You return home" + "\n" + "unharmed, but 80 gold poorer. The adrenaline is pumping, so you decide to go on another" + "\n" + "adventure immediately. You get an extra turn.",
+        "On your newest adventure you decide to go through the forest, as you're walking along" + "\n" + "the path, you accidentally walk over a trap, and fall into a pit. After being in there for a" + "\n" + "while, someone appears at the top. They will help you out, but only if you pay them 50" + "\n" + "gold. You give in, and can finally get out.",
+        "You’re on your daily stroll, when you see something glinting in the distance. You" + "\n" + "approach and see that it’s a goldmine, and it seems that there’s no one else here. You go" + "\n" + "over and grab all the gold you can carry and go home. Once you go home, you get to" + "\n" + "counting it, and see that you’ve managed to find 650 gold. YOU’RE RICH!",
     };
     
     private static String rules = "Rules";
