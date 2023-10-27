c5eede96a55d6a9df5f43bacddbea019e032c323
Marcus Lemser <mle91@protonmail.com>
2023-10-27 11:27:51 +0200

    Most JUnit made

--- DiceGameTest.java
+++ DiceGameTest.java
@@ -5,16 +5,43 @@ import org.junit.*;
 public class DiceGameTest {
     
     @Test
-    public void test1(){
-        boolean test = true;
-        var game = new DiceGame();
-        game.creatDie();
-        game.createPlayer();
-        
-        for(int i = 0; i < 1000; i++){
-            game.roll(1);
-            
-        }
-        
+    public void threeThousandShouldWin(){
+        var game = new DiceGame(2);
+        game.createPlayerArr();
+        game.setPlayerBalance(3000, 1);
+        assertEquals(true, game.verifyWin(1));
+    }
+
+    @Test
+    public void twoThousandNinetyNineShouldNotWin(){
+        var game = new DiceGame(2);
+        game.createPlayerArr();
+        game.setPlayerBalance(2999, 1);
+        assertEquals(false, game.verifyWin(1));
+    }
+
+    @Test
+    public void playerBalanceShouldReturnToZeroIfNegativeAfterRoll(){
+        var game = new DiceGame(2);
+        game.createPlayerArr();
+        game.setPlayerBalance(-10000.0, 1);
+        assertEquals(0.0, game.getPlayerBalance(1), 0);
+    }
+
+    @Test
+    public void playerBalanceShouldReturnToZeroIfNegativeOne(){
+        var game = new DiceGame(2);
+        game.createPlayerArr();
+        game.setPlayerBalance(-1.0, 1);
+        assertEquals(0.0, game.getPlayerBalance(1), 0);
+    }
+
+    @Test
+    public void playerBalanceShouldReturnToZeroIfGoesBelow(){
+        var game = new DiceGame(2);
+        game.createPlayerArr();
+        game.setPlayerBalance(1000.0, 1);
+        game.addPlayerBalance(-10001, 1);
+        assertEquals(0.0, game.getPlayerBalance(1), 0);
     }
 }
